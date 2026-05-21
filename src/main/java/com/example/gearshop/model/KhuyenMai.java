c8545b63986e7af9d596f633a4ecb8e8271f451d
haei <dangthiha20012004@gmail.com>
2026-05-21 19:46:07 +0700

    Update KhuyenMai.java

--- src/main/java/com/example/gearshop/model/KhuyenMai.java
+++ src/main/java/com/example/gearshop/model/KhuyenMai.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class KhuyenMai {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     private String maKM;
     private String tenKM;
